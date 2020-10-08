1ee2939e21166ee0bdd90bf4269a058e98f1b1f4
Hertanto Irawan <hertanto.irawan@outlook.com>
2020-10-09 01:27:51 +0700

    fix date optional filters

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const formatDate = require('date-fns/format'); 
+const isDateValid = require('date-fns/isValid');
+const parseISO = require('date-fns/parseISO');
+const addDays = require('date-fns/addDays')
 
 const cors = require('cors')
 
@@ -67,10 +71,19 @@ app.post('/api/exercise/add', (req, res) => {
       return;    
     }
     else {
-        let activityDate = req.body.date;
-        if (activityDate === '') activityDate = new Date();
+        if (!isNumeric(req.body.duration)) {
+          res.send("Invalid exercise duration");
+          return;
+        }
 
-        //TODO: add date and druation validation
+        let activityDate = req.body.date;
+        if (activityDate === '') {
+          activityDate = new Date();
+        }
+        else if (!isValidDate(activityDate)) {
+          res.send("Invalid exercise date");
+          return;
+        }        
 
         let activity = new Exercise({ 
           userId: req.body.userId,
@@ -81,10 +94,10 @@ app.post('/api/exercise/add', (req, res) => {
         activity.save((err, doc) => {
           if (err) return console.error(err); //TODO: log error properly
 
-          res.json({
+          res.json({ 
             "_id": doc._id,
             "username": user.username,
-            "date": doc.date,
+            "date": formatDate(doc.date, 'iii MMM dd yyyy'),
             "duration": doc.duration,
             "description": doc.description
           });   
@@ -106,49 +119,75 @@ app.get('/api/exercise/log', (req, res) => {
     else {
       let fromDate = req.query.from;
       let toDate = req.query.to;
-      let limit = (typeof req.query.limit === 'undefined') ? 0 : req.query.limit;
+
+      let limit = ((req.query.limit === '') || !isNumeric(req.query.limit)) ? 0 : req.query.limit;
 
       let filter = {};
       filter.userId = req.query.userId;
 
-      //check date format and ignore if not valid
-      //TODO: make this if-statements more elegant
-      if ((fromDate !== '') && (toDate !== '')) {
-        filter.date = {
-          $gte: (fromDate === '') ? undefined : new Date(new Date(fromDate).setHours(00, 00, 00)),
-          $lt: (toDate === '') ? null : new Date(new Date(toDate).setHours(23, 59, 59)) //TODO: change to from date + 1
-        };    
+      // TODO: make this if-statements more elegant
+      let filterFromDate = '', filterToDate = '';
+      if (fromDate !== '') {
+        if (isValidDate(fromDate)) {
+          filterFromDate = new Date(new Date(fromDate).setHours(00, 00, 00));
+
+          if (filterFromDate !== '') {
+            filter.date = {};
+            filter.date.$gte = filterFromDate;
+          }
+        }
+        else {
+          fromDate = '';
+        }
       }
-      else if (fromDate !== '') {
-        filter.date = {
-          $gte: (fromDate === '') ? undefined : new Date(new Date(fromDate).setHours(00, 00, 00)),
-        };    
-      }
-      else if (toDate !== '') {
-        filter.date = {
-          $lt: (toDate === '') ? null : new Date(new Date(toDate).setHours(23, 59, 59)) //TODO: change to from date + 1
-        };    
+      if (toDate !== '') {
+        if (isValidDate(toDate)) {
+          filterToDate = new Date(new Date(toDate).setHours(00, 00, 00));
+          filterToDate = addDays(filterToDate, 1);
+
+          if (filterToDate !== '') {
+            if (filter.date === undefined) {
+              filter.date = {};
+            }
+            filter.date.$lt = filterToDate;
+          }    
+        }
+        else {
+          toDate = '';
+        }
       }
 
       Exercise
       .find(filter, "description duration date -_id")
       .limit(parseInt(limit))
+      .lean()
       .exec((err, result) => {
         if (err) return console.error(err); //TODO: log error properly
+
+        // change date format to 'Sat Sep 05 2020'
+        for (let a=0; a < result.length; a++) {
+          result[a].date = formatDate(result[a].date, 'iii MMM dd yyyy');
+        }
 
         res.json({
           "_id": req.query.userId,
           "username": user.username,
-          "from": (fromDate === '') ? undefined : fromDate,
-          "to": (toDate === '') ? undefined : toDate,
+          "from": (fromDate === '') ? undefined : formatDate(new Date(fromDate), 'iii MMM dd yyyy'),
+          "to": (toDate === '') ? undefined : formatDate(new Date(toDate), 'iii MMM dd yyyy'),
           "count": result.length,
-          "log": result //TODO: change date format to 'Sat Sep 05 2020'
+          "log": result
         });
       });
     }    
   });
 });
 
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+function isValidDate(d) {
+  return isDateValid(parseISO(d));
+}
 
 // Not found middleware
 app.use((req, res, next) => {
