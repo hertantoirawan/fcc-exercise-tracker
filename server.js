cf110d9779917193f072faea375b189ce20c3810
Hertanto Irawan <hertanto.irawan@outlook.com>
2020-10-10 17:52:45 +0700

    fix _id result after adding exercise

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ app.get('/', (req, res) => {
 
 // TODO: move to a separate model file
 const Athlete = mongoose.model('Athlete', new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true
+  }
 }));
 
 app.post('/api/exercise/new-user', (req, res) => {
@@ -38,14 +41,14 @@ app.post('/api/exercise/new-user', (req, res) => {
   Athlete.count(filter, (err, count) => { 
     if (count > 0){
       res.send("Username already taken");    
+      return;
     }
-    else {
-      let hero = new Athlete({ username: req.body.username });
-      hero.save(function(err, doc) {
-        if (err) return console.error(err); //TODO: log error properly
-        res.json({"username": doc.username,"_id": doc._id});   
-      });
-    }
+
+    let hero = new Athlete({ username: req.body.username });
+    hero.save(function(err, doc) {
+      if (err) return console.error(err); //TODO: log error properly
+      res.json({"username": doc.username,"_id": doc._id});   
+    });
   });
 });
 
@@ -63,8 +66,14 @@ const Exercise = mongoose.model('Exercise', new mongoose.Schema({
     type: mongoose.Types.ObjectId, // TODO: might need to be ObjectId type, instead of String
     required: true
   },
-  description: String,
-  duration: Number,
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
   date: Date,
 }));
 
@@ -74,39 +83,39 @@ app.post('/api/exercise/add', (req, res) => {
       res.send("Unknown userId");
       return;    
     }
-    else {
-        if (!isNumeric(req.body.duration)) {
-          res.send("Invalid exercise duration");
-          return;
-        }
 
-        let activityDate = req.body.date;
-        if ((activityDate === '') || (activityDate === undefined)) {
-          activityDate = new Date();
-        }
-        else if (!isValidDate(activityDate)) {
-          res.send("Invalid exercise date");
-          return;
-        }        
-
-        let activity = new Exercise({ 
-          userId: mongoose.Types.ObjectId(req.body.userId),
-          description: req.body.description,
-          duration: Number(req.body.duration),
-          date: activityDate,
-        });
-        activity.save((err, doc) => {
-          if (err) return console.error(err); //TODO: log error properly
-          
-          res.json({ 
-            "_id": doc._id,
-            "username": user.username,
-            "date": formatDate(doc.date, 'iii MMM dd yyyy'),
-            "duration": Number(doc.duration),
-            "description": doc.description
-          });   
-        });
+    if (!isNumeric(req.body.duration)) {
+      res.send("Invalid exercise duration");
+      return;
     }
+
+    let activityDate = req.body.date;
+    if ((activityDate === '') || (activityDate === undefined)) {
+      activityDate = new Date();
+    }
+    else if (!isValidDate(activityDate)) {
+      res.send("Invalid exercise date");
+      return;
+    }        
+
+    let activity = new Exercise({ 
+      userId: mongoose.Types.ObjectId(req.body.userId),
+      description: req.body.description,
+      duration: Number(req.body.duration),
+      date: activityDate,
+    });
+
+    activity.save((err, doc) => {
+      if (err) return console.error(err); //TODO: log error properly
+      
+      res.json({ 
+        "_id": user._id,
+        "username": user.username,
+        "date": formatDate(doc.date, 'iii MMM dd yyyy'),
+        "duration": doc.duration,
+        "description": doc.description
+      });   
+    });
   });
 });
 
@@ -120,69 +129,68 @@ app.get('/api/exercise/log', (req, res) => {
       res.send("Unknown userId");
       return;    
     }
-    else {
-      let fromDate = req.query.from;
-      let toDate = req.query.to;
 
-      let limit = ((req.query.limit === '') || !isNumeric(req.query.limit)) ? 0 : req.query.limit;
+    let fromDate = req.query.from;
+    let toDate = req.query.to;
 
-      let filter = {};
-      filter.userId = mongoose.Types.ObjectId(req.query.userId);
+    let limit = ((req.query.limit === '') || !isNumeric(req.query.limit)) ? 0 : req.query.limit;
 
-      // TODO: make this if-statements more elegant
-      let filterFromDate = '', filterToDate = '';
-      if (fromDate !== '') {
-        if (isValidDate(fromDate)) {
-          filterFromDate = new Date(new Date(fromDate).setHours(00, 00, 00));
+    let filter = {};
+    filter.userId = mongoose.Types.ObjectId(req.query.userId);
 
-          if (filterFromDate !== '') {
+    // TODO: make this if-statements more elegant
+    let filterFromDate = '', filterToDate = '';
+    if (fromDate !== '') {
+      if (isValidDate(fromDate)) {
+        filterFromDate = new Date(new Date(fromDate).setHours(00, 00, 00));
+
+        if (filterFromDate !== '') {
+          filter.date = {};
+          filter.date.$gte = filterFromDate;
+        }
+      }
+      else {
+        fromDate = '';
+      }
+    }
+    if (toDate !== '') {
+      if (isValidDate(toDate)) {
+        filterToDate = new Date(new Date(toDate).setHours(00, 00, 00));
+        filterToDate = addDays(filterToDate, 1);
+
+        if (filterToDate !== '') {
+          if (filter.date === undefined) {
             filter.date = {};
-            filter.date.$gte = filterFromDate;
           }
-        }
-        else {
-          fromDate = '';
-        }
+          filter.date.$lt = filterToDate;
+        }    
       }
-      if (toDate !== '') {
-        if (isValidDate(toDate)) {
-          filterToDate = new Date(new Date(toDate).setHours(00, 00, 00));
-          filterToDate = addDays(filterToDate, 1);
+      else {
+        toDate = '';
+      }
+    }
 
-          if (filterToDate !== '') {
-            if (filter.date === undefined) {
-              filter.date = {};
-            }
-            filter.date.$lt = filterToDate;
-          }    
-        }
-        else {
-          toDate = '';
-        }
+    Exercise
+    .find(filter, "description duration date -_id")
+    .limit(parseInt(limit))
+    .lean()
+    .exec((err, result) => {
+      if (err) return console.error(err); //TODO: log error properly
+
+      // change date format to 'Sat Sep 05 2020'
+      for (let a=0; a < result.length; a++) {
+        result[a].date = formatDate(result[a].date, 'iii MMM dd yyyy');
       }
 
-      Exercise
-      .find(filter, "description duration date -_id")
-      .limit(parseInt(limit))
-      .lean()
-      .exec((err, result) => {
-        if (err) return console.error(err); //TODO: log error properly
-
-        // change date format to 'Sat Sep 05 2020'
-        for (let a=0; a < result.length; a++) {
-          result[a].date = formatDate(result[a].date, 'iii MMM dd yyyy');
-        }
-
-        res.json({
-          "_id": req.query.userId,
-          "username": user.username,
-          "from": (fromDate === '') ? undefined : formatDate(new Date(fromDate), 'iii MMM dd yyyy'),
-          "to": (toDate === '') ? undefined : formatDate(new Date(toDate), 'iii MMM dd yyyy'),
-          "count": result.length,
-          "log": result
-        });
+      res.json({
+        "_id": req.query.userId,
+        "username": user.username,
+        "from": (fromDate === '') ? undefined : formatDate(new Date(fromDate), 'iii MMM dd yyyy'),
+        "to": (toDate === '') ? undefined : formatDate(new Date(toDate), 'iii MMM dd yyyy'),
+        "count": result.length,
+        "log": result
       });
-    }    
+    });
   });
 });
 
